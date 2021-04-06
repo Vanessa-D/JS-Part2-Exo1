e447f8c576c88d966be8fc5a44d65b1c005c2c5d
Vanessa-D <vanessa.ducloux@laposte.net>
2021-04-06 13:20:22 +0200

    first

---
+++ main.js
@@ -0,0 +1,6 @@
+// Fonction qui dans le DOM va chercher la source de l'image à afficher au passage de la souris 
+// puis pour un temps indéterminé jusqu'au rafraîchissement de la page
+// à l'endroit de l'id image
+function setNewImage(){
+    document.getElementById("img1").src="images/image1_2.jpg";
+}
